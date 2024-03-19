64b800de77a4299409b5140bda55b35076c2d8bc
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2024-03-19 02:04:33 -0400

    update dth

--- dth.py
+++ dth.py
@@ -29,7 +29,7 @@ def get_prediction_at(X, t, sub_learners):
     sub_learner_predictions = []
     for sub_learner in sub_learners:
         sub_learner_predictions.append(sub_learner.model.predict([X]))
-    return np.mean(sub_learner_predictions), 1/(np.std(sub_learner_predictions)+0.00001)
+    return np.mean(sub_learner_predictions), np.std(sub_learner_predictions)
 
 class DTH:
     ''''' Time needs to be the first feature in the input data. '''''
@@ -42,11 +42,13 @@ class DTH:
                  num_cold_start_samples=10,
                  pruning_disabled=False, 
                  num_pruning_threads=5,
-                 max_elimination = 10):
+                 max_elimination = 10,
+                 use_sublearners_as_baselearner = False,
+                 max_investigated_samples = 50):
         ### hyper-parameters
         self.base_learner = learning_models.DecissionTree(max_depth=5)
         self.num_sub_learners = num_sub_learners
-        self.sub_model = learning_models.DecissionTree(max_depth=2)  # sub-models are used to estimate the uncertainty of the base_learner, they need to be relatively fast or be only a few
+        self.sub_model = learning_models.DecissionTree(max_depth=5)  # sub-models are used to estimate the uncertainty of the base_learner, they need to be relatively fast or be only a few
         self.epsilon = epsilon
         self.min_new_samples_for_base_learner_update = min_new_samples_for_base_learner_update
         self.min_new_samples_for_pruining = min_new_samples_for_pruining
@@ -55,7 +57,8 @@ class DTH:
         self.pruning_disabled = pruning_disabled
         self.num_pruning_threads = num_pruning_threads
         self.max_elimination = max_elimination
-
+        self.use_sublearners_as_baselearner = use_sublearners_as_baselearner
+        self.max_investigated_samples = max_investigated_samples
         #### initialization
         self.method_name = 'DTH'
         self.sub_learners = []
@@ -85,7 +88,10 @@ class DTH:
     
 
     def predict_online_model(self, X):
-        return self.base_learner.predict(X)
+        if self.use_sublearners_as_baselearner:
+            return get_prediction_at(X, self.current_time, self.sub_learners)[0]
+        else:
+            return self.base_learner.predict(X)
 
 
     def update_online_model(self):
@@ -93,6 +99,8 @@ class DTH:
             self.new_samples_count_for_base_learner_update = 0
             # fit the base_learner to the memory
             self.fit_base_learner()
+            
+
 
         # if self.num_cold_start_samples < len(self.memory) and self.new_samples_count_for_pruining >= self.min_new_samples_for_pruining:
         if self.new_samples_count_for_pruining >= self.min_new_samples_for_pruining:
@@ -110,7 +118,8 @@ class DTH:
                         
                     else:
                         if self.first_time:
-                            print('pruning without concurrent...')
+                            # print('pruning without concurrent...')
+                            pass
                         self.prune_memory()
                     # self.prune_memory_concurrent()
                 
@@ -119,51 +128,62 @@ class DTH:
 
     def fit_base_learner(self):
         # fit the base_learner to the memory
-        if len(self.memory) == 0:
-            print('no samples in memory to fit the base_learner')
-        # else:
-            # print('fitting the base_learner with ', len(self.memory), ' samples')
-        # X, y = self.samples2xy(self.memory)
-        # make X and y numpy arrays where X includes the time
-        X = np.array([np.append(sample[0],sample[2]) for sample in self.memory])
-        y = np.array([sample[1] for sample in self.memory])
-        self.base_learner.model.fit(X, y)
-        self.base_learner_is_fitted = True
+        
+
+        if self.use_sublearners_as_baselearner:
+            if len(self.memory) >= self.num_sub_learners:
+                # print('not enough samples in memory to fit the base_learner')
+                self.update_sub_learners()
+                self.base_learner_is_fitted = True
+        else:
+            if len(self.memory) < 1:
+                print('no samples in memory to fit the base_learner')
+            # else:
+                # print('fitting the base_learner with ', len(self.memory), ' samples')
+            # X, y = self.samples2xy(self.memory)
+            # make X and y numpy arrays where X includes the time
+            X = np.array([np.append(sample[0],sample[2]) for sample in self.memory])
+            y = np.array([sample[1] for sample in self.memory])
+            self.base_learner.model.fit(X, y)
+            self.base_learner_is_fitted = True
 
 
     def prune_memory(self):
-        # update sub-learners if memory is large enough
-        
+        # print('pruning memory...')  
         self.update_sub_learners()
+
+        
         
         # assess the validity of the samples in memory
         for i, (X, y, t) in enumerate(self.memory):
+            if i > self.max_investigated_samples:
+                break
             elimination_count = 0
             # stability_at_sampling_time = self.get_prediction_stability_at(X, t)
             # stability_at_current_time = self.get_prediction_stability_at(X, self.current_time)    
             # prediction_at_sampling_time = self.predict_online_model(X, t)
             # prediction_at_current_time = self.predict_online_model(X, self.current_time)
 
-            prediction_at_current_time, stability_at_current_time = get_prediction_at(X, self.current_time, self.sub_learners)
-            prediction_at_sampling_time, stability_at_sampling_time = get_prediction_at(X, t, self.sub_learners)
-            
+            mu_c, sigma_c = self.get_prediction_at(X, self.current_time)
+            mu_o, sigma_o = self.get_prediction_at(X, t)
 
-            prediction_at_between_time, stability_at_between_time = get_prediction_at(X, (t+self.current_time)/2, self.sub_learners)
 
-            
-            # set alpha as the difference of the two predictions average and the between time prediction
-            # alpha = np.abs((prediction_at_sampling_time+prediction_at_current_time)/2)
+            # calculate the probability of y being sampled from the distribution at the current time
+            prob_y_current = np.exp(-0.5 * (y - mu_c)**2 / sigma_c**2) / (sigma_c * np.sqrt(2 * np.pi))
 
-            # alpha = np.abs(prediction_at_sampling_time*stability_at_sampling_time - prediction_at_current_time*stability_at_current_time)
-            # if alpha < self.epsilon:
-            if stability_at_current_time > self.epsilon:
-                if min(prediction_at_sampling_time, prediction_at_current_time) < prediction_at_between_time < max(prediction_at_sampling_time, prediction_at_current_time):
-                    # self.memory.remove(sample)
-                    self.memory.pop(i)
-                    elimination_count += 1
-                    if elimination_count > self.max_elimination:
-                        self.update_sub_learners()
-                        elimination_count = 0
+            # calculate the probability of y being sampled from the distribution at the sampling time
+            prob_y_original = np.exp(-0.5 * (y - mu_o)**2 / sigma_o**2) / (sigma_o * np.sqrt(2 * np.pi))
+
+            prob_original_given_y = (prob_y_original / (prob_y_original + prob_y_current))
+
+            # print('prob_expired:', prob_original_given_y)
+            if prob_original_given_y > self.epsilon:
+                self.memory.pop(i)
+                # print('eliminating sample with prob:', prob_original_given_y)
+                elimination_count += 1
+                if elimination_count > self.max_elimination:
+                    self.update_sub_learners()
+                    elimination_count = 0
     
     def prune_memory_concurrent(self):
         K = self.num_pruning_threads
@@ -248,22 +268,46 @@ class DTH:
             sub_learner_predictions.append(sub_learner.model.predict([X]))
         return 1/(np.std(sub_learner_predictions)+0.00001)
 
+    def get_prediction_at(self, X, t):
+        # add time to the features 
+        X = np.append(X, t)
+        # print('X:', X)
+        sub_learner_predictions = []
+        for sub_learner in self.sub_learners:
+            sub_learner_predictions.append(sub_learner.model.predict([X]))
+        return np.mean(sub_learner_predictions), np.std(sub_learner_predictions)
+
 
     def predict_online_model(self, X, t):
-        if self.base_learner_is_fitted:
-            X_time_included = np.append(X, t)
-            return self.base_learner.predict(X_time_included)
-        elif len(self.memory) > 0:
-            X_time_included = np.append(X, t)
-            # X, y = self.samples2xy(self.memory)
-            X = np.array([np.append(sample[0],sample[2]) for sample in self.memory])
-            y = np.array([sample[1] for sample in self.memory])
-
-            self.base_learner.model.fit(X, y)
-            self.base_learner_is_fitted = True
-            return self.base_learner.predict(X_time_included)
-        else: # raise error 'no model is fitted'
-            return 0
+        if self.use_sublearners_as_baselearner:
+            if self.base_learner_is_fitted:
+                return self.get_prediction_at(X, self.current_time)[0]
+            else:
+                if len(self.memory) > self.num_sub_learners:
+                    X = np.array([np.append(sample[0],sample[2]) for sample in self.memory])
+                    y = np.array([sample[1] for sample in self.memory])
+                    self.base_learner.model.fit(X, y)
+                    self.base_learner_is_fitted = True
+                    return self.predict_online_model(X, t)
+                elif len(self.memory) > 0:
+                    # return mean of the y values in the memory
+                    return np.mean([sample[1] for sample in self.memory])
+                else:
+                    return 0
+        else: # use a separate base_learner
+            if self.base_learner_is_fitted:
+                X_time_included = np.append(X, t)
+                return self.base_learner.predict(X_time_included)
+            elif len(self.memory) > 0:
+                # X_time_included = np.append(X, t)
+                # X, y = self.samples2xy(self.memory)
+                X_time_included = np.array([np.append(sample[0],sample[2]) for sample in self.memory])
+                y = np.array([sample[1] for sample in self.memory])
+                self.base_learner.model.fit(X_time_included, y)
+                self.base_learner_is_fitted = True
+                return self.predict_online_model(X, t)
+            else: # raise error 'no model is fitted'
+                return 0
     
 
 
